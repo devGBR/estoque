d2c077b6c76fad48e09212fa989affd81c21effa
devGBR <gfcontroladoriageral@gmail.com>
2022-08-05 12:25:27 -0300

    Stylized pg

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List Productos</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            }
+    </style>
 </head>
 <body>
     <h1>Listagem de produtos com Laravel</h1>
