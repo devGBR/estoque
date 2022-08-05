889bd4d80fb2f18ebe7d8f71900ee627f9bcd8db
devGBR <gfcontroladoriageral@gmail.com>
2022-08-05 13:57:30 -0300

    boostrap

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -6,11 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Controle de estoque</title>
     <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('fonts/glyphicons-halflings-regular.woff2') }}" rel="stylesheet">
 </head>
 <body> 
     <div class="container">
 
         <h1>Listagem de produtos com Laravel</h1>
+        <button type="button" class="btn btn-default" aria-label="Left Align">
+  <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+</button>
         <table class="table table-hover table-bordered table-striped">
             <?php foreach ($produtos as $p): ?>
         <tr>
@@ -19,12 +23,15 @@
             <td><?= $p->descricao ?></td>
             <td><?= $p->quantidade ?></td>
             <td>
-                <a href="/produtos/mostra/<?= $p->nome ?>">visualizar</a>
+                <a href="/produtos/mostra/<?= $p->nome ?>">
+                <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                </a>
             </td>
         </tr>
         <?php endforeach ?>
         </table>
 
     </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
