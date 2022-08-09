7b44140b104c9b45516f0dd2486e3f36462e08fb
devGBR <gfcontroladoriageral@gmail.com>
2022-08-09 13:09:41 -0300

    verification the table

--- resources/views/listagem.blade.php
+++ resources/views/listagem.blade.php
@@ -1,25 +1,28 @@
 @extends('principal')
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
 @section('conteudo')
-        <h1>Listagem de produtos</h1>
-        <table class="table table-hover table-bordered table-striped">
-            @forelse($produtos as $p): 
-        <tr>
-            <td>{{$p->nome}}</td>
-            <td>{{$p->valor}}</td>
-            <td>{{$p->descricao}}</td>
-            <td>{{$p->quantidade}}</td>
-            <td>
-                <a href="/produtos/mostra/<?= $p->id ?>">
-                    <span class = "glyphicon glyphicon-search"></sapn>
-                </a>
-            </td>
-            @empty
-                <tr>
-                    <td colspan="500">Nenhum produto cadastrado</td>
-                </tr>
-        </tr>
-        @endforelse
-        </table>
 
+        @if(empty($produtos))
+            <div class="alert alert-danger">
+                Você não tem nenhum produto cadastrado.
+            </div>
+        @else
+
+            <h1>Listagem de produtos</h1>
+                <table class="table table-hover table-bordered table-striped">
+                    @foreach ($produtos as $p): 
+                        <tr>
+                            <td>{{$p->nome}}</td>
+                            <td>{{$p->valor}}</td>
+                            <td>{{$p->descricao}}</td>
+                            <td>{{$p->quantidade}}</td>
+                            <td>
+                                <a href="/produtos/mostra/<?= $p->id ?>">
+                                    <span class = "glyphicon glyphicon-search"></span>
+                                </a>
+                            </td>
+                       </tr>
+                    @endforeach
+            </table>
+        @endif
 @stop
